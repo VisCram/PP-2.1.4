f6895d874f01b48ebf59b4b300557a68d6e63993
VisCram <126577181+VisCram@users.noreply.github.com>
2023-09-04 19:46:24 +0500

    Add Dog and Timer Beans

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,7 +1,9 @@
 package app.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 @ComponentScan(basePackages = "app")
